8cac799a829c98c2e2779be80aa766ed018620ea
Asad404 <110449014+AsadTeamHail@users.noreply.github.com>
2023-02-17 19:02:14 +0500

    push

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require('cors');
+const app = express();
+const bodyParser = require('body-parser');
+var morgan = require('morgan');
+const db = require("./models");
+
+// const verify = require('./functions/tokenVerification');
+
+//const { Items } = require("./functions/associations/shopAssociation")
+
+app.use(morgan('tiny'));
+
+app.use(cors());
+app.use(bodyParser.urlencoded({limit: '100mb', extended: true}));
+app.use(bodyParser.json({limit: '100mb', extended: true}));
+app.use(express.json());
+db.sequelize.sync();
+
+app.get("/", (req, res) => { res.json('Welcome to MavDocs Server') });
+
+// app.get("/getUser", verify, (req, res) => { res.json({isLoggedIn:true, username:req.body.username}) });
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+    console.log('Press Ctrl+C to quit.');
+});
---
+++ models/Users.js
@@ -0,0 +1,35 @@
+module.exports = (sequelize, DataTypes) => {
+    const Users = sequelize.define("Users", {
+        id:{
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey:true,
+            allowNull: false,
+            validate:{
+                notEmpty: true
+            }
+        },
+        fullname:{
+            type:DataTypes.STRING,
+            allowNull: false,
+            validate:{
+                notEmpty: true
+            }
+        },
+        company:{
+            type:DataTypes.STRING,
+            allowNull: false,
+            validate:{
+                notEmpty: true
+            }
+        },
+        phone:{
+            type:DataTypes.INTEGER,
+            allowNull: false,
+            validate:{
+                notEmpty: true
+            }
+        },
+    })
+    return Users
+}
