f78a182829529d6d5860b26bf26d964770de028b
Asad404 <110449014+AsadTeamHail@users.noreply.github.com>
2023-02-17 19:20:27 +0500

    push

---
+++ routes/auth/index.js
@@ -0,0 +1,168 @@
+const routes = require('express').Router();
+const jwt = require('jsonwebtoken');
+const Sib = require('sib-api-v3-sdk');
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
+
+const { Users, ShopUsers } = require('../../models');
+
+const name = (x, otp, sub) => {
+  const client = Sib.ApiClient.instance;
+  const apiKey = client.authentications['api-key'];
+  apiKey.apiKey = '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';
+  const transEmailApi = new Sib.TransactionalEmailsApi();
+  const sender = { email:'syedabdullahteamhail@gmail.com',name:'Syed Abdullah'};
+  const recievers = [ { email:x, }, ];
+
+  transEmailApi.sendTransacEmail({
+    sender,
+    to: recievers,
+    subject:sub,
+    //textContent:'Wishing you a warm welcome to Hail Technologies',
+    htmlContent:`<p>Your Account has been successfully setup</p>
+      <p>Enter the following code in the login screen</p>
+      <h1>{{params.pass}}</h1>
+      <br/>
+      <p>Do not share this code with anyone else.</p>
+      <br/>
+      <p>Regards</p>
+      <p>Support Team</p>`,
+    params:{
+        pass:otp,
+    },
+  }).then((x)=>console.log(x))
+  .catch((e)=>console.log(e));
+}
+
+routes.post("/verification", async(req, res)=>{
+    const { email, pass, type } = req.body
+
+    if(type=="customer"){
+
+      if(req.body.email && req.body.pass){
+        const users = await Users.findOne({where:{email:email, password:pass}})
+        if(users){
+          if(email==users.email && pass==users.password){
+            const payload = { type:type, picture:users.profile_pic, fullname:`${users.fullname} ${users.l_name}`,loginId:`${users.id}` }
+            jwt.sign(
+              payload,
+              'qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnm',
+              {expiresIn:"8760h"},
+              (err,token) => {
+                if(err) return res.json({message: err})
+                  return res.json({
+                      message:"Success",
+                      token: "BearerSplit"+token
+                    })
+                  }
+                )
+          } else {  return res.json({message:"Invalid"}) }
+        }
+        else {  return res.json({message:"Invalid"})  }
+      } else {  return res.json({message:"Invalid"})  }
+
+    }else if(type=="shopowner"){
+
+      if(req.body.email && req.body.pass){
+        const users = await ShopUsers.findOne({where:{email:email, password:pass}})
+        if(users){
+          if(email==users.email && pass==users.password){
+            const payload = { picture:users.profile_pic, username:`${users.f_name} ${users.l_name}`,loginId:`${users.id}`, type:type }
+            jwt.sign(
+              payload,
+              'qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnm',
+              {expiresIn:"8760h"},
+              (err,token) => {
+                if(err) return res.json({message: err})
+                  return res.json({
+                      message:"Success",
+                      token: "BearerSplit"+token
+                    })
+                  }
+                )
+          } else {  return res.json({message:"Invalid"}) }
+        }
+        else {  return res.json({message:"Invalid"})  }
+      } else {  return res.json({message:"Invalid"})  }
+
+    }
+
+});
+
+routes.post("/signUp", async(req, res)=>{
+
+  const otp = Math.floor(100000 + Math.random() * 900000);
+
+  const { phone, fullname, company, type } = req.body;
+  try {
+    if(type=="customer"){
+      const customerVerification = await Users.findOne({where:{email:email}});
+        if(customerVerification){
+          res.send('Already Exists');
+        }else{
+          const customer = await Users.create({
+            f_name:f_name, l_name:l_name, email:email,role:'customer', password:otp
+          });
+          name(customer.email, otp, 'Welcome To Innovatory');
+          res.json({status:'success',customer});
+        }
+    }else if(type=="rider"){
+      
+      const riderVerification = await Users.findOne({where:{email:email}});
+        if(riderVerification){
+          res.json({status:'error', message:"Already Exists!"});
+        } else {
+          console.log('Rider Creation')
+          const Rider = await Users.create({
+            fullname:req.body.fullname,
+            company:req.body.company,
+            phone:req.body.phone
+          })
+          name(Rider.email, otp, 'Welcome To MavDocs');
+          res.json({status:'success'});
+        }
+    }else{
+      res.json({status:'error', message:"Something Went Wrong Please Try Again"});
+    }
+  }
+  catch (error) {
+    res.json({status:'error', message:"Something Went Wrong Please Try Again"});
+  }
+});
+
+// routes.post("/login", async(req, res)=>{
+
+//   const otp = Math.floor(100000 + Math.random()*900000);
+//   const { email, type } = req.body;
+//   try {
+//     if(type=="customer") {
+//       const customerVerification = await Users.findOne({where:{email:email}});
+//       if(customerVerification){
+//         const customer = await Users.update({password:otp},{where:{id:customerVerification.id}});
+//         console.log(customer);
+//         name(customerVerification.email, otp, 'Innovatory OTP');
+//         res.status(200).json(customerVerification)
+//         // res.json({status:'success'},customerVerification);
+//       }else{
+//         res.json({status:'error'});
+//       }
+//     } else if(type=="rider") {
+//       const shopOwnerVerification = await ShopUsers.findOne({where:{email:email}});
+//         if(shopOwnerVerification){
+//           await ShopUsers.update({password:otp},{where:{id:shopOwnerVerification.id}});
+//           name(shopOwnerVerification.email, otp, 'Innovatory OTP');
+//           res.json({status:'success'});
+//         } else {
+//           res.json({status:'error'});
+//         }
+//     }
+//     else {
+//       res.json({status:'error'});
+//     }
+//   }
+//   catch (error) {
+//     res.send(error);
+//   }
+// });
+
+module.exports = routes;
